cbd62572564bdd6c0ef336209f834faaaf124b1d
vernonthedev <techjaja2@gmail.com>
2024-05-29 05:59:41 -0700

    #Update: setup the github auth creds

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23liqjvpLLkoRw3ST9';
+const githubClientSecret = '6494dea776161ae200070a52cc086772c8073cb0';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
